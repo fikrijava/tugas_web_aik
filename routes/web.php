b2d65144d0cded0a529e36b3362c48736a478b86
fikrijava <email_fatwaalfiqri@gmail.com>
2026-07-16 19:39:48 +0700

    Sempurnaa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/tata-cara-sholat', [HomeController::class, 'guide'])->name('guide');
+Route::get('/video-stream', [HomeController::class, 'streamVideo'])->name('video.stream');
